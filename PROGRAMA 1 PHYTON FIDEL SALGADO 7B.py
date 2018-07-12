97bac9cfdb311a78e9224f7732cdc73e4bdfeac7
salgadofidel <40905385+salgadofidel@users.noreply.github.com>
2018-07-11 20:42:28 -0500

    NETACAD PHYTON CAPITULO I
    programa calcula el área de un triángulo, solicita ingreso de dos variables tiene comentarios e impresiones en pantalla

---
+++ PROGRAMA 1 PHYTON FIDEL SALGADO 7B.py
@@ -0,0 +1,20 @@
+﻿print('***********************************************')# IMPRESION EN PANTALLA
+print('********* UNIVERSIDAD ISRAEL ******************')
+print('*** FIDEL SALGADO      SEPTIMO NIVEL B ********')
+print('****** PROGRAMA BASICO PHYTON  NETACAD ********')
+print('***********************************************')
+
+#PROGRAMA BASICO PHYTON PERMITE CONOCER LAS BASES DEL LENGUAJE DE PROGRAMACION
+#ESTO ES UN COMENTARIO
+
+#PROGRAMA QUE LE PERMITE CONOCER EL AREA DE UN TRIANGULO
+print('CALCULO DEL AREA DE UN TRIANGULO')
+print(' Area de un triangulo= base * altura / 2')
+print(' ')
+b=float(input('INGRESE LA BASE :'));print(b)
+a=float(input('INGRESE LA ALTURA :'));print(a)
+r=(b*a)/2
+print('EL AREA DEL TRIANGULO ES:',r)
+print('BUEN TRABAJO')
+print('ADIOS')
+
